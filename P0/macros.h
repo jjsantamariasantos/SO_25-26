f3ace92aac4cd8aaa45dae518c6ad4349de2d251
jjsantamariasantos <j.j.ssantos@udc.es>
2025-09-15 11:07:30 +0200

    me equivoque donde ponerlo xd

--- P0/macros.h
+++ P0/macros.h
@@ -2,9 +2,14 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+// macros for general use
 #define MAX_INPUT_SIZE 1024
 #define SNULL -1
 #define UNUSED(f) do { (void)(f); } while (0) // to avoid warnings of unused variables
 #define ARGS_MAX 10
+// end of macros for general use
 
+//macros for help texts
+
+//end of macros for help texts
 #endif  /* MACROS_H */
